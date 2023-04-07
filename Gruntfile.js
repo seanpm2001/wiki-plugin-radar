75139ae80557124b1f2c7a3985893c40d5d9b575
Paul Rodwell <paul.rodwell@btinternet.com>
2023-04-07 15:06:47 +0100

    tidy gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,4 @@
 module.exports = function (grunt) {
-  grunt.loadNpmTasks('grunt-contrib-coffee');
-  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-git-authors');
 
   grunt.initConfig({
@@ -11,29 +9,6 @@ module.exports = function (grunt) {
         "Stephen Judkins <stephen.judkins@gmail.com>",
         "Nick Niemeir <nick.niemeir@gmail.com>"
       ]
-    },
-
-    coffee: {
-      client: {
-        expand: true,
-        options: {
-          transpile: { presets: ['@babel/preset-env'] },
-          sourceMap: true
-        },
-        src: ['client/*.coffee'],
-        ext: '.js'
-      }
-    },
-
-    watch: {
-      all: {
-        files: ['client/*.coffee'],
-        tasks: ['coffee']
-      }
     }
   });
-
-  grunt.registerTask('build', ['coffee']);
-  grunt.registerTask('default', ['build']);
-
 };
